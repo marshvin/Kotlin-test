46927cdf30f9b3e26020b18adfa11409c9f0b8a9
Vincent Marsh <vincentmarsh01@gmail.com>
2022-10-14 21:12:58 +0300

    learning

---
+++ trial5.kt
@@ -0,0 +1,11 @@
+fun main()
+{
+    val names = listOf("Ross","Chandler", "Monica","Racheal")
+    for (names in 1..5){
+        println(names)
+    }
+    for (names in 1 until 5){
+        println(names)
+    }
+}
+
